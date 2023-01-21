e36d57506ddafd7ca72d15a34e36bd43ece8c3e6
VarionDrakon <52794446+VarionDrakon@users.noreply.github.com>
2023-01-21 21:48:13 +0300

    The first version of the console program
    Ru:
    Программа для учёта посетителей проходящих через КПП. Имеет возможности смены профиля и простую запись в текстовые файлы. При записи учитывает время и дату (Автоматически), цель и ФИО посетителя (Вручную вписывается), при выходе посетителя указывается только ФИО вписывается, остальное подставляется автоматически, проект создан на .NetCore 6 консольное приложение. Проект создан в целях ознакомления и изучения таких принципов ООП в C# как - Абстракция, Наследование, Полиморфизм и Инкапсуляция.
    En:
    A program for tracking visitors passing through the checkpoint. It has the ability to change the profile and a simple entry into text files. When recording, it takes into account the time and date (Automatically), the purpose and the visitor's full name (Manually entered), when the visitor exits, only the full name is entered, the rest is substituted automatically, the project is created on .NETCore 6 is a console application. The project was created in order to familiarize and study such principles of OOP in C# as Abstraction, Inheritance, Polymorphism and Encapsulation.

---
+++ KPP accounting log/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Diagnostics;
+
+namespace KPP_accounting_log
+{
+    public class DateTimeStorage
+    {
+        public string? nowEnter;
+        public void UpdateDateTimeAuthorization()
+        {
+            nowEnter = DateTime.Now.ToString("ddMMyyyyHHmmss") + "note.log";
+        }
+    }
+    public abstract class LoginTemplate : DateTimeStorage
+    {
+        public string? FullName;
+        public string? Purpose;
+    }
+    internal class Program : LoginTemplate
+    {
+        public string? NameProfileAuthorization { private get; set; }
+        readonly string[] arrLogPassArray = File.ReadAllLines(@"Users/token.txt");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting app...");
+
+            var anInstanceProgramClass = new Program();
+            anInstanceProgramClass.MainConsoleWindow();
+        }
+        private void MainConsoleWindow()
+        {
+            Console.Clear();
+            WorkplaceAuthorization();
+        }
+        private void WorkplaceAuthorization()
+        {
+            if (Directory.Exists(@"Users/") == false || File.Exists(@"Users/token.txt") == false)
+            {
+                Console.WriteLine("Вы не можете начать работу так как отсутствует директория или учётный лист пользователей, обратитесь к системному администратору для решения проблемы!");
+            }
+            else
+            {
+                Console.WriteLine("Для начала работы, пожалуйста, авторизируйтесь. Введите логин, затем нажмите enter и введите пароль, после чего повторно нажмите enter!");
+                string? employeeName = Console.ReadLine();
+                NameProfileAuthorization = employeeName;
+                Console.WriteLine("Теперь введите пароль!");
+                try
+                {
+                    int? employeePass = Convert.ToInt32(Console.ReadLine());
+
+                    bool allowCorrectLog = arrLogPassArray.Contains(employeeName);
+                    bool allowCorrectPass = arrLogPassArray.Contains(employeePass.ToString());
+
+                    if (allowCorrectLog == true)
+                    {
+                        if (allowCorrectPass == true)
+                        {
+                            UpdateDateTimeAuthorization();
+                            EnteringCredentials();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неправильный пароль! Повторите попытку.");
+                            WorkplaceAuthorization();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неправильный логин! Повторите попытку.");
+                        WorkplaceAuthorization();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("ОШИБКА! Пароль вводится только цифрами! Повторите попытку.");
+                    WorkplaceAuthorization();
+                }
+            }
+        }
+        private void EnteringCredentials()
+        {
+            Console.Clear();
+            Console.WriteLine("Что Вы хотите сделать? \n 1. Записать вход \n 2. Записать выход \n 3. Войти в другую учётную запись \n 4. Выйти из программы");
+            string? number = Console.ReadLine();
+
+            if (Directory.Exists(@"Log/") == false)
+            {
+                Directory.CreateDirectory("Log");
+            }
+            if (number == "1")
+            {
+                Console.WriteLine("ВВедите ФИО:");
+                FullName = Console.ReadLine();
+                Console.WriteLine("ВВедите цель входа:");
+                Purpose = Console.ReadLine();
+                using (StreamWriter writeLog = new StreamWriter(@"Log/" + NameProfileAuthorization + nowEnter, true))
+                {
+                    writeLog.WriteLine("ФИО: " + FullName + " Время: " + DateTime.Now + " Цель входа: " + Purpose);
+                }
+                EnteringCredentials();
+            }
+            else if (number == "2")
+            {
+                Console.WriteLine("ВВедите ФИО:");
+                FullName = Console.ReadLine();
+                using (StreamWriter writeLog = new StreamWriter(@"Log/" + NameProfileAuthorization + nowEnter, true))
+                {
+                    writeLog.WriteLine("ФИО: " + FullName + " Время: " + DateTime.Now);
+                }
+                EnteringCredentials();
+            }
+            else if (number == "3")
+            {
+                MainConsoleWindow();
+            }
+            else if (number == "4")
+            {
+                Process.GetCurrentProcess().Kill();
+            }
+            else
+            {
+                Console.WriteLine("Invalid number! Retry attempt");
+                EnteringCredentials();
+            }
+        }
+    }
+}
